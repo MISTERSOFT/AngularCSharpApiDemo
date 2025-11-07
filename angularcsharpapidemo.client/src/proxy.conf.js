4e63935bea20e55a04404a00385b35806b271be6
MISTERSOFT <MISTERSOFT@users.noreply.github.com>
2025-11-07 16:23:39 +0100

    Ajoutez des fichiers projet.

---
+++ angularcsharpapidemo.client/src/proxy.conf.js
@@ -0,0 +1,19 @@
+const { env } = require('process');
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:32769';
+
+console.log('TARGET NIOGGER:', target)
+
+const PROXY_CONFIG = [
+  {
+    context: [
+       "/api"
+       //"/weatherforecast",
+     ],
+    target,
+    secure: false
+  }
+]
+
+module.exports = PROXY_CONFIG;
